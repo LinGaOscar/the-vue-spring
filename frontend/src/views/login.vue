e0da4fe98bc91674e7a26c7bcb3de4ec845b2c82
LinGaOscar <oscar987123@yahoo.com.tw>
2022-01-04 17:44:14 +0800

    set

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,29 +1,30 @@
 <template>
   <body id="login-page">
-    <el-form class="login-container" label-position="left" label-width="0px">
-      <h3 class="login_title">系統登入</h3>
-      <el-form-item>
-        <el-input
-          type="text"
-          v-model="loginForm.loginName"
-          auto-complete="off"
-          placeholder="賬號"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input
-          type="password"
-          v-model="loginForm.password"
-          auto-complete="off"
-          placeholder="密碼"
-        ></el-input>
-      </el-form-item>
-      <el-form-item style="width: 100%">
-        <el-button type="primary" style="width: 100%;  border: none"
-          >登入</el-button
-        >
-      </el-form-item>
-    </el-form>
+  <el-form class="login-container" label-position="left" label-width="0px">
+    <h3 class="login_title">系統登入</h3>
+    <el-form-item>
+      <el-input
+        type="text"
+        v-model="loginForm.loginName"
+        auto-complete="off"
+        placeholder="賬號"
+      ></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input
+        type="password"
+        v-model="loginForm.password"
+        auto-complete="off"
+        placeholder="密碼"
+      ></el-input>
+    </el-form-item>
+    <el-form-item style="width: 100%">
+      <el-button type="primary" style="width: 100%;  border: none"
+      >登入
+      </el-button
+      >
+    </el-form-item>
+  </el-form>
   </body>
 </template>
 
@@ -45,16 +46,17 @@ export default {
 
 <style scoped>
 #login-page {
-  background: url("../assets/logo.png") no-repeat;
-  background-position: center;
+  background: url("../assets/logo.png") no-repeat center;
   height: 100%;
   width: 100%;
   background-size: cover;
   position: fixed;
 }
+
 body {
   margin: 0px;
 }
+
 .login-container {
   border-radius: 15px;
   background-clip: padding-box;
