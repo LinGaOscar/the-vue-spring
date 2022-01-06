73b8075237d9b7c245f5be9f75ded6cc570e34c0
LinGaOscar <oscar987123@yahoo.com.tw>
2022-01-06 13:28:59 +0800

    reset

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,23 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
-
-const axios = require('axios');
-Vue.prototype.$axios = axios
-axios.defaults.baseURL = 'http://localhost:8005/api'
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,31 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Login from '@/views/login.vue'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
+const routes = [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+        path: '/',
+        name: 'Home',
+        component: () => import('@/views/Home.vue')
     },
     {
-      path: '/login',
-      name: 'Login',
-      component: Login
+        path: '/about',
+        name: 'About',
+        component: () => import('@/views/About.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import ('@/views/Login.vue')
+
     }
-  ]
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
+
+export default router
